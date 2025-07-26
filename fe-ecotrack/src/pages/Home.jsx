b3c9e33e889efd5960ae45654b131d41dbc03106
TechLine08 <techcart87@gmail.com>
2025-07-26 22:22:34 +0700

    fix for home 2

--- fe-ecotrack/src/pages/Home.jsx
+++ fe-ecotrack/src/pages/Home.jsx
@@ -64,7 +64,7 @@ export default function Home() {
       }
     });
     return () => unsubscribe();
-  }, [fetchLeaderboardgit ]);
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
