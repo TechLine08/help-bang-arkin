9412f601e74ac2f1b3f945863705519371d26227
TechLine08 <techcart87@gmail.com>
2025-07-26 16:23:38 +0700

    🎉 Initial deployable frontend commit

--- fe-ecotrack/tailwind.config.js
+++ fe-ecotrack/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"], // ✅ Required for CRA + Tailwind v2
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
