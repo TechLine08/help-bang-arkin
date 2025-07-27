f98b4c83a1acd24b82b358a9774da46c46c75d20
TechLine08 <techcart87@gmail.com>
2025-07-27 20:32:35 +0700

    update on home

--- fe-ecotrack/src/pages/Home.jsx
+++ fe-ecotrack/src/pages/Home.jsx
@@ -252,7 +252,7 @@ export default function Home() {
                     </div>
                     <div>
                       <p className="font-medium text-gray-800">
-                        {leaderboardScope === 'country' ? item.country : item.users?.display_name || item.user_id}
+                        {leaderboardScope === 'country' ? item.country : item.users?.display_name}
                       </p>
                     </div>
                   </div>
